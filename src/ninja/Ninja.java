37298ace8884a7907652d38d61728314852ef3df
League Student <league@iMac-109.attlocal.net>
2020-02-24 16:56:25 -0800

    Ninja

--- src/ninja/Ninja.java
+++ src/ninja/Ninja.java
@@ -1,5 +1,23 @@
 package ninja;
 
-public class Ninja {
-	
-}
+	public class Ninja {
+
+		/* Assume the Enemy class exists and has the required methods. */
+		String alliegence;
+		Enemy enemy;
+		     Ninja(Enemy enemy, String alliegence) {
+		          this.alliegence = alliegence;
+		          this.enemy = enemy;
+		     }
+
+		     void sabotage() {
+		          infiltrate();
+		          setOnFire(enemy.getLocation());
+		     }
+
+
+		     void infiltrate() {
+		          Ninja nagato = new Ninja(new Enemy(), "Nagato");
+		          Ninja momochi = new Ninja(new Enemy(), "Momochi");
+		     }
+		}
